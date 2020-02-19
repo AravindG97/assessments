fb01ccb95dd16c951a9b978ac60f92196ff34a7a
AravindG97 <60167699+AravindG97@users.noreply.github.com>
2020-02-19 14:34:08 +0530

    Add files via upload

--- python/practise and assessments/assessment2.py
+++ python/practise and assessments/assessment2.py
@@ -16,7 +16,7 @@ list1=list(list1)
 
 def rate(a,b):
   x=country.index(a)
-  print("the equivalent currency for "+str(b)+" INR : "+str("%.2f" %(b/list1[x]))+str(currency[x]))
+  print("the equivalent currency for "+str(b)+" INR : "+str("%.2f" %(b/list1[x]))+" "+str(currency[x]))
 
 
 def timee(a):
@@ -37,14 +37,26 @@ def timee(a):
   print("current time in "+str(country[aa])+": "+actual_time.strftime("%H %M")+" "+str(time_zone[aa]))
 
 def display(a):
-  """[key:country]:[values:  index:0 = language   index:1=currency]"""
-  aa = list (zip (language, currency_rate))
-  bb = dict (zip (country, aa))
-  for x, y in bb.items ():
-    if x == a:
-      print("country: ",x)
-      print("language: ",y[0])
-      print ("currency value: " + str(y[1]) + " INR")
+  dic={}
+  for x in range (len (country)) :
+    dic[country[x]] = {}
+  aa = 0
+  for x in dic :
+    for i in range (len (time_zone)) :
+      if i == aa :
+        dic[x]["time_zone"] = time_zone[i]
+        dic[x]["time"] = time[i]
+        dic[x]["currency"] = currency[i]
+        dic[x]["language"] = language[i]
+        dic[x]["currency_rate"] = currency_rate[i]
+      else :
+        continue
+    aa += 1
+  for x,y in dic.items():
+    if x==a:
+      for i,j in y.items():
+        print(i+" : "+str(j))
+
 
 con=input("enter the country from uk,us,mexico,australia: ")
 val=int(input("enter the amount: "))
